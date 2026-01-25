01f989e015bb40a98dcbc031a702dca3af632346
Akihiro Nishiyama <aki16gaf@outlook.jp>
2026-01-25 20:25:29 +0900

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.tsx'
 import Header from './component/Header'
 import "bootstrap/dist/css/bootstrap.min.css";
-import MainForm from './component/MainForm.tsx'
+// import MainForm from './component/MainForm.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
